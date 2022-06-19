750308fa73ed4e1e5b4056ab83e6f68c68f684b5
RFields24 <omellyinc24@gmail.com>
2022-06-19 12:01:00 -0400

    added TeamRow component to allow multiple teams to show in container edited css accordingly

---
+++ src/TeamRow.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./App.css"
+import Ylogo from './img/ylogo.png'
+import Yname from './img/yname.png'
+
+
+function TeamRow() {
+  return (
+    <div className="team-row">
+      <div className="team-info">
+          <div className="team-logo-container">
+            <img className="logo" src={Ylogo} alt="" />
+          </div>
+          <div className="team-name">
+            <h2>Yankees</h2>
+            <img className="name-logo" src={Yname} alt="" />
+          </div>  
+        </div>
+        <div className="stats">
+          <div className="wins">
+            <h2>3</h2>
+          </div>
+          <div className="losses">
+            <h2>3</h2>
+          </div>
+          <div className="gb">
+            <h2>-</h2>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default TeamRow
--- src/TeamStandings.js
+++ src/TeamStandings.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import "./App.css"
-import Ylogo from './img/ylogo.png'
-import Yname from './img/yname.png'
+import TeamRow from "./TeamRow"
 
 
 
@@ -9,26 +8,11 @@ import Yname from './img/yname.png'
 function TeamStandings() {
   return (
     <div className="team-standings-container">
-      <div className="team-info">
-        <div className="team-logo-container">
-          <img className="logo" src={Ylogo} alt="" />
-        </div>
-        <div className="team-name">
-          <h2>Yankees</h2>
-          <img className="name-logo" src={Yname} alt="" />
-        </div>  
-      </div>
-      <div className="stats">
-        <div className="wins">
-          <h2>3</h2>
-        </div>
-        <div className="losses">
-          <h2>3</h2>
-        </div>
-        <div className="gb">
-          <h2>-</h2>
-        </div>
-      </div>
+      <TeamRow />
+      <TeamRow />
+      <TeamRow />
+      <TeamRow />
+      <TeamRow />
     </div>
   )
 }
